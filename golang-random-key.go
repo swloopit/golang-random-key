b180d5b7596abf3efd2a0a4378d165d8b483de0b
Alex <info@swloop.it>
2018-08-02 23:08:06 +0200

    Add files via upload

---
+++ golang-random-key.go
@@ -0,0 +1,22 @@
+package golang-random-key
+import(
+       "math/rand"
+       "time"
+)
+/**/
+func CreateRandomKey(size int) string{
+     s:=""
+     for i:=1;i<=size;i++{
+         s+=GetSingleRandomCharacter()
+     }
+     return s
+}
+
+func GetSingleRandomCharacter() string{
+     rand.Seed(time.Now().UnixNano())
+     alphabet:="abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+     k:=string(alphabet[rand.Intn(len(alphabet))])
+     return k
+}
+
+
